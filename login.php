5f194737d3b491e76b27d4e34c0c4351ae2b3f36
Deepthi1409 <gsd1401@gmail.com>
2021-03-26 12:32:47 +0530

    login backend

--- login.php
+++ login.php
@@ -4,35 +4,27 @@ require('config.php');
 
 if(isset($_POST['login']))
 {
-    $email = mysqli_real_escape_string($conn,$_POST['email']);
-    $pwd = mysqli_real_escape_string($conn,$_POST['password']);
+   $email = $_POST['email'];
+   $pwd = $_POST['password'];
 
-    $sql_e = "SELECT * FROM user WHERE Email = '$email'";
-    $res_e = mysqli_query($conn,$sql_e);
-    $sql_p = "SELECT * FROM user WHERE Password = '".md5($pwd)."'";
-    $res_p = mysqli_query($conn,$sql_p);
+   $pass = md5($pwd);
+   
+   $sql = "SELECT * FROM user WHERE Email ='".$email."' AND Password = '".$pass."'";
 
-    if($res_e)
-    {
-        if($res_p)
-        {
-            echo "<script type='text/javascript'>alert('Loggedin Successfully!');
-            window.location='home.html';
-            </script>";
-        }
-        else
-        {
-            echo "<script type='text/javascript'>alert('Enter a valid password!');
-            window.location='login.html';
-            </script>";
-        }
-    }
-    else
-    {
-        echo "<script type='text/javascript'>alert('Enter a valid email!');
-        window.location='login.html';
-        </script>"; 
-    }
+   $res = mysqli_query($conn,$sql);
+
+   if(mysqli_num_rows($res) == 1)
+   {
+     echo "<script type='text/javascript'>alert('Loggedin successfully!');
+     window.location='home.html';
+     </script>";
+   }
+   else
+   {
+     echo "<script type='text/javascript'>alert('Enter the valid credentials!');
+     window.location='login.html';
+     </script>";
+   }
 }
 
 ?>
